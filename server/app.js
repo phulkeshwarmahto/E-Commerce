5732af7dbe86179b793e76483e6c4b970b6f4113
Phulkeshwar Mahto <phulkeshwar.e@gmail.com>
2026-04-26 23:09:28 +0530

    fixed product card

--- server/app.js
+++ server/app.js
@@ -19,7 +19,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: process.env.CLIENT_URL || "http://localhost:5173",
+    origin: process.env.CLIENT_URL || "http://localhost:5175",
   }),
 );
 app.use(express.json({ limit: "2mb" }));
